5031b1067b74b7f1f4dd3169cabd3e4f53774cda
George Kryukov <91170890+Hlompy@users.noreply.github.com>
2026-07-06 16:46:47 +0300

    Update payment_test.go

--- internal/app/domain/payment_test.go
+++ internal/app/domain/payment_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/evt/recruitment-task-payment-gateway/internal/app/domain"
+	"github.com/Hlompy/task-payment/internal/app/domain"
 )
 
 func TestNewPaymentIntent_Valid(t *testing.T) {
