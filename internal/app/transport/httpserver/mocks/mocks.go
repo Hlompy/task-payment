226d1ecb9787eb610979d829ab90e93e33da1c96
George Kryukov <91170890+Hlompy@users.noreply.github.com>
2026-07-06 16:52:52 +0300

    Update mocks.go

--- internal/app/transport/httpserver/mocks/mocks.go
+++ internal/app/transport/httpserver/mocks/mocks.go
@@ -3,7 +3,7 @@ package mocks
 import (
 	"context"
 
-	"github.com/evt/recruitment-task-payment-gateway/internal/app/domain"
+	"github.com/Hlompy/task-payment/internal/app/domain"
 )
 
 type MockPaymentService struct {
